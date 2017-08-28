1cda5c39b66c5054b26347d662afd758c0dd555b
LemonBoy <thatlemon@gmail.com>
2017-08-28 23:02:11 +0200

    Reorganize a bit the code
    Add more tests

--- blake2.scm
+++ blake2.scm
@@ -13,48 +13,51 @@
   (er-macro-transformer
     (lambda (f r c)
       (let-optionals (map strip-syntax (cdr f))
-        ((name #f) (init #f) (init-key #f) (update #f) (final #f) (state-size 0) (digest-length 0) (key-length 0) (block-size 0))
-        `(define ,(symbol-append name '-primitive)
-           (lambda (#!key (length ,digest-length) key)
-             (unless (and (exact? length) (> length 0) (<= length ,digest-length))
-               (error "the digest length is invalid"))
-             (when key
-               (unless (or (string? key) (blob? key))
-                 (error "the key parameter must be a blob or a string"))
-               (unless (and (> (##sys#size key) 0) (<= (##sys#size key) ,key-length))
-                 (error "the key parameter is too long or too short")))
-             (make-message-digest-primitive
-               ; size of the state block
-               ,state-size
-               ; length of the digest
-               length
-               ; init
-               (if key
+        ((prefix #f) (state-size 0) (digest-length 0) (key-length 0) (block-size 0))
+        (let ((primitive-name (symbol-append prefix '-primitive))
+              (init-proc      (symbol-append prefix '_init))
+              (init-key-proc  (symbol-append prefix '_init_key))
+              (update-proc    (symbol-append prefix '_update))
+              (final-proc     (symbol-append prefix '_final)))
+          `(define ,primitive-name
+             (lambda (#!key (length ,digest-length) key)
+               (unless (and (exact? length) (> length 0) (<= length ,digest-length))
+                 (error ',primitive-name "the digest length is out of bounds" 1 ,digest-length))
+               (when key
+                 (unless (or (string? key) (blob? key))
+                   (error ',primitive-name "the key parameter must be a blob or a string"))
+                 (unless (and (> (##sys#size key) 0) (<= (##sys#size key) ,key-length))
+                   (error ',primitive-name "the key parameter is too long or too short")))
+               (make-message-digest-primitive
+                 ; size of the state block
+                 ,state-size
+                 ; length of the digest
+                 length
+                 ; init
+                 (if key
+                     (lambda (ctx)
+                       ((foreign-lambda int ,init-key-proc c-pointer size_t scheme-pointer size_t) ctx length key (##sys#size key)))
                    (lambda (ctx)
-                     ((foreign-lambda int ,init-key c-pointer size_t scheme-pointer size_t) ctx length key (##sys#size key)))
-                   (lambda (ctx)
-                     ((foreign-lambda int ,init c-pointer size_t) ctx length)))
-               ; update
-               (foreign-lambda int ,update c-pointer scheme-pointer size_t)
-               ; finalize
-               (lambda (ctx out)
-                 ((foreign-lambda int ,final c-pointer scheme-pointer size_t) ctx out length))
-               ; block size
-               ,block-size
-               ; name
-               ',(symbol-append name '-primitive)
-               ; raw-update
-               (foreign-lambda int ,update c-pointer c-pointer size_t))))))))
+                     ((foreign-lambda int ,init-proc c-pointer size_t) ctx length)))
+                 ; update
+                 (foreign-lambda int ,update-proc c-pointer scheme-pointer size_t)
+                 ; finalize
+                 (lambda (ctx out)
+                   ((foreign-lambda int ,final-proc c-pointer scheme-pointer size_t) ctx out length))
+                 ; block size
+                 ,block-size
+                 ; name
+                 ',primitive-name
+                 ; raw-update
+                 (foreign-lambda int ,update-proc c-pointer c-pointer size_t)))))))))
 
 (define-impl blake2s
-  blake2s_init blake2s_init_key blake2s_update blake2s_final
   (foreign-type-size "blake2s_state")
   (foreign-value "BLAKE2S_OUTBYTES" unsigned-int)
   (foreign-value "BLAKE2S_KEYBYTES" unsigned-int)
   (foreign-value "BLAKE2S_BLOCKBYTES" unsigned-int))
 
 (define-impl blake2b
-  blake2b_init blake2b_init_key blake2b_update blake2b_final
   (foreign-type-size "blake2b_state")
   (foreign-value "BLAKE2B_OUTBYTES" unsigned-int)
   (foreign-value "BLAKE2B_KEYBYTES" unsigned-int)
--- tests/run.scm
+++ tests/run.scm
@@ -1,6 +1,15 @@
 (use test)
 (use message-digest-bv blake2)
 
+(test-group "Parameter validation"
+  (test-error "zero length" (blake2b-primitive length: 0))
+  (test-error "negative length" (blake2b-primitive length: -1))
+  (test-error "inexact length" (blake2b-primitive length: 1.0))
+  (test-error "huge length" (blake2b-primitive length: 1000))
+  (test-error "empty key" (blake2b-primitive key: ""))
+  (test-error "huge key" (blake2b-primitive key: (make-string 100)))
+)
+
 (test-group "RFC7693 examples"
   (test "blake2b"
         "ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923"
